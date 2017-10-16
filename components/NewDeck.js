51ce5bb697221bc9576ffaab09ab81a0a62db362
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-10-16 14:28:35 +0200

    Add deck interface done

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux'
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import TextButton from './TextButton'
 
 
-export default class NewDeck extends React.Component {
+class NewDeck extends React.Component {
+  addDeck = () => {
+    console.log('Adding Deck');
+  };
+
   render() {
     return (
-      <View >
-       <Text> This is new decks madafaka</Text>
+      <View style={styles.deck}>
+       <View style={styles.box}><Text>Add new Deck</Text></View>
+       <View style={styles.box}>
+         <TextInput  />
+       </View>
+       <View style={styles.box}>
+       <TextButton style={styles.default} onPress={this.addDeck}>
+            Add Deck
+        </TextButton>
+      </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  deck: {
+    flex: 1,
+    alignItems: 'stretch',
+    marginLeft: 1,
+    marginRight: 1,
+  },
+  box: {
+    alignItems: 'center',
+    height: 50,
+    margin: 10,
+  },
+  default: {
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: '#aaa',
+    padding: 4
+  }
+})
+
+function mapStateToProps (state, { navigation }) {
+  return {
+  }
+}
+
+function mapDispatchToProps (dispatch, { navigation }) {
+  return {
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(NewDeck);
